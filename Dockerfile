c992e2ae2192b35e20d167baa7ffa409fbeda0d5
Raja Simon <6974101+rajasimon@users.noreply.github.com>
2020-05-22 13:01:23 +0530

    Increment version to nearest possible
    1. Pump pylint both django and pylint version.
    2. Increase Channels version to 2.2
    3. Increase nodejs to node10 in Dockerfile
    4. Add auto format settings.py file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3.6
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-RUN apt-get update
 RUN curl -sL https://deb.nodesource.com/setup_10.x | bash -
 RUN apt-get install nodejs -y
 COPY requirements/ /usr/src/app/requirements/
--- settings/settings.py
+++ settings/settings.py
@@ -6,26 +6,24 @@ except ImportError:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, '../statics')
+STATIC_ROOT = os.path.join(BASE_DIR, "../statics")
 
 # channels settings
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis", 6379)],
-        },
+        "CONFIG": {"hosts": [("redis", 6379)],},
     },
 }
 # ANYMAIL : mailgun configuration
 ANYMAIL = {
     "MAILGUN_API_KEY": "< your api key at mailgun >",
-    "MAILGUN_SENDER_DOMAIN": "< your sender domain at mailgun >"
+    "MAILGUN_SENDER_DOMAIN": "< your sender domain at mailgun >",
 }
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+EMAIL_BACKEND = "anymail.backends.mailgun.MailgunBackend"
 DEFAULT_FROM_EMAIL = " <<your default from email>>"
